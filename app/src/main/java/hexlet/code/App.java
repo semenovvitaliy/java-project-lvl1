9015bbf97b35e1825ff50f4a7ae331ee355aa020
Vitalii Semenov <semvi@list.ru>
2022-06-12 11:48:53 +0300

    Step 2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,17 @@
 package hexlet.code;
 
+import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        App.Cli();
+    }
+
+    public static void Cli() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String str = in.nextLine();
+        System.out.println("Hello, " + str + "!");
     }
 }
