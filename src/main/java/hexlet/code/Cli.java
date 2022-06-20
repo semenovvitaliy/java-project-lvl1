0fd295ffddfcec03cafdd362b38a7ef71c0d2c2c
Vitalii Semenov <semvi@list.ru>
2022-06-20 21:38:36 +0300

    reload repository with gitignore

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void cli() {
+        System.out.println("Welcome to the Brain Games!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String str = in.nextLine();
+        System.out.println("Hello, " + str + "!");
+    }
+}
